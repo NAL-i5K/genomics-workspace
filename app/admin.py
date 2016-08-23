10cfaf478168e2eb9c4bde9b603c8fd4e974d4f5
ifish <ifish@moon.csie.ntu.edu.tw>
2016-08-23 01:34:12 -0400

    update move_organism_to_app

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
+from django.forms import ModelForm
+from suit.widgets import AutosizedTextarea
+from .models import *
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -13,7 +15,35 @@ class ProfileInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(UserAdmin):
     inlines = (ProfileInline, )
-    
+   
+
+class OrganismForm(ModelForm):
+    class Meta:
+        widgets = {
+            'description': AutosizedTextarea(attrs={'rows': 10, 'class': 'input-xxlarge'}),
+        }
+
+class OrganismAdmin(admin.ModelAdmin):
+    form = OrganismForm
+    list_display = ('display_name', 'short_name', 'tax_id', 'short_description',)
+    search_fields = ('display_name', 'short_name', 'tax_id', 'description',)
+    actions_on_top = True
+    actions_on_bottom = True
+
+    def short_description(self, obj):
+        if len(obj.description) < 100:
+            return obj.description
+        else:
+            return obj.description[:100] + '...'
+    short_description.short_description = 'description'
+    class Media:
+        css = {
+            'all': ('blast/css/organism-admin.css',)
+        }
+        js = ('blast/scripts/organism-admin.js',)
+admin.site.register(Organism, OrganismAdmin)
+
+ 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- hmmer/models.py
+++ hmmer/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from blast.models import Organism
+from app.models import Organism
 from filebrowser.fields import FileBrowseField
 import os.path
 
