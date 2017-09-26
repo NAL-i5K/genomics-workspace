29b00555c6e9f5f7ed418314a6e88448d2ec7d21
Yi Hsiao <hsiaoyi0504@gmail.com>
2017-09-26 11:56:47 +0800

    specify celery app name in tests

--- i5k/tests.py
+++ i5k/tests.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 import celery
+from celery import Celery
 import subprocess
+import multiprocessing
 
 class CeleryTestCase(SimpleTestCase):
     def test_rabbitmq_run(self):
@@ -15,3 +17,15 @@ class CeleryTestCase(SimpleTestCase):
     def test_celery_run(self):
         d = celery.app.control.inspect().stats()
         self.assertIsNotNone(d)
+
+    def test_celery_beat_run(self):
+        pass
+
+    def test_celery_worker_process(self):
+        app = Celery('i5k')
+        keys = celery.app.control.inspect(app=app).stats().keys()
+        num_node = len(keys)
+        self.assertEqual(num_node, 1)
+        num_cpu = multiprocessing.cpu_count()
+        num_prpocess = len(celery.app.control.inspect().stats()[keys[0]]['pool']['processes'])
+        self.assertEqual(num_prpocess, num_cpu)
