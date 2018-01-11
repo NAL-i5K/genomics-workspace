61da461fed70e0576e1ff5c3719827b627eed3fd
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-01-11 16:20:26 -0500

    fix user without login can see other query result

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,3 @@
-'''
-    Dashboard views.py
-'''
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
@@ -60,72 +57,72 @@ def dashboard(request):
                 return render(request, 'dashboard/index.html')
         elif relative_path == 'blast_hist':
             search_list = []
-            for obj in BlastSearch.objects.filter(user=request.user.id):
-                search_dict = {}
-                id_num += 1
-                orgs = json.loads(obj.organisms)
-                orglist = orgs.split()
-                forgs = orglist[0]
-                seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
-                date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
-                search_dict['search_head']     = '%s  -  %s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program, forgs)
-                search_dict['id_str']          = 'collapsible' + str(id_num)
-                search_dict['task_id']         = obj.task_id
-                search_dict['search_tag']      = obj.search_tag
-                search_dict['soft_masking']    = obj.soft_masking
-                search_dict['enqueue_date']    = obj.enqueue_date
-                search_dict['low_complexity']  = obj.low_complexity
-                search_dict['penalty']         = obj.penalty
-                search_dict['evalue']          = obj.evalue
-                search_dict['gapopen']         = obj.gapopen
-                search_dict['strand']          = obj.strand
-                search_dict['program']         = obj.program
-                search_dict['reward']          = obj.reward
-                search_dict['gapextend']       = obj.gapextend
-                search_dict['word_size']       = obj.word_size
-                search_dict['max_target_seqs'] = obj.max_target_seqs
-                search_dict['sequence']        = seq
-                search_dict['organisms']       = orgs
-                search_list.append(search_dict)
+            if request.user.is_authenticated():
+                for obj in BlastSearch.objects.filter(user=request.user.id):
+                    search_dict = {}
+                    id_num += 1
+                    orgs = json.loads(obj.organisms)
+                    orglist = orgs.split()
+                    forgs = orglist[0]
+                    seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
+                    date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
+                    search_dict['search_head']     = '%s  -  %s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program, forgs)
+                    search_dict['id_str']          = 'collapsible' + str(id_num)
+                    search_dict['task_id']         = obj.task_id
+                    search_dict['search_tag']      = obj.search_tag
+                    search_dict['soft_masking']    = obj.soft_masking
+                    search_dict['enqueue_date']    = obj.enqueue_date
+                    search_dict['low_complexity']  = obj.low_complexity
+                    search_dict['penalty']         = obj.penalty
+                    search_dict['evalue']          = obj.evalue
+                    search_dict['gapopen']         = obj.gapopen
+                    search_dict['strand']          = obj.strand
+                    search_dict['program']         = obj.program
+                    search_dict['reward']          = obj.reward
+                    search_dict['gapextend']       = obj.gapextend
+                    search_dict['word_size']       = obj.word_size
+                    search_dict['max_target_seqs'] = obj.max_target_seqs
+                    search_dict['sequence']        = seq
+                    search_dict['organisms']       = orgs
+                    search_list.append(search_dict)
             return render(request, 'dashboard/blast_hist.html', { 'search_list': search_list})
         elif relative_path == 'hmmer_hist':
-           search_list = []
-           for obj in HmmerSearch.objects.filter(user=request.user.id):
-               search_dict = {}
-               id_num += 1
-               orgs = json.loads(obj.organisms)
-               orglist = orgs.split()
-               forgs = orglist[0]
-               seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
-               date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
-               search_dict['search_head']     = '%s  -  %s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program, forgs)
-               search_dict['id_str']          = 'collapsible' + str(id_num)
-               search_dict['task_id']         = obj.task_id
-               search_dict['search_tag']      = obj.search_tag
-               search_dict['enqueue_date']    = obj.enqueue_date
-               search_dict['program']         = obj.program
-               search_dict['sequence']        = seq
-               search_dict['organisms']       = orgs
-               search_list.append(search_dict)
-           return render(request, 'dashboard/hmmer_hist.html', { 'search_list': search_list})
+            if request.user.is_authenticated():
+                for obj in HmmerSearch.objects.filter(user=request.user.id):
+                    search_dict = {}
+                    id_num += 1
+                    orgs = json.loads(obj.organisms)
+                    orglist = orgs.split()
+                    forgs = orglist[0]
+                    seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
+                    date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
+                    search_dict['search_head']     = '%s  -  %s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program, forgs)
+                    search_dict['id_str']          = 'collapsible' + str(id_num)
+                    search_dict['task_id']         = obj.task_id
+                    search_dict['search_tag']      = obj.search_tag
+                    search_dict['enqueue_date']    = obj.enqueue_date
+                    search_dict['program']         = obj.program
+                    search_dict['sequence']        = seq
+                    search_dict['organisms']       = orgs
+                    search_list.append(search_dict)
+            return render(request, 'dashboard/hmmer_hist.html', { 'search_list': search_list})
         elif relative_path == 'clustal_hist':
-           search_list = []
-           for obj in ClustalSearch.objects.filter(user=request.user.id):
-               search_dict = {}
-               id_num += 1
-               seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
-               date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
-               search_dict['search_head']     = '%s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program)
-               search_dict['id_str']          = 'collapsible' + str(id_num)
-               search_dict['task_id']         = obj.task_id
-               search_dict['search_tag']      = obj.search_tag
-               search_dict['enqueue_date']    = obj.enqueue_date
-               search_dict['program']         = obj.program
-               search_dict['sequence']        = obj.sequence
-               search_list.append(search_dict)
-           return render(request, 'dashboard/clustal_hist.html', { 'search_list': search_list})
-
-
+            search_list = []
+            if not request.user.is_authenticated():
+                for obj in ClustalSearch.objects.filter(user=request.user.id):
+                    search_dict = {}
+                    id_num += 1
+                    seq = (obj.sequence[:20] + '..') if len(obj.sequence) > 20 else obj.sequence
+                    date_str = obj.enqueue_date.strftime('%b %d %H:%M:%S')
+                    search_dict['search_head']     = '%s  -  %s  -  %s' % (obj.search_tag, date_str, obj.program)
+                    search_dict['id_str']          = 'collapsible' + str(id_num)
+                    search_dict['task_id']         = obj.task_id
+                    search_dict['search_tag']      = obj.search_tag
+                    search_dict['enqueue_date']    = obj.enqueue_date
+                    search_dict['program']         = obj.program
+                    search_dict['sequence']        = obj.sequence
+                    search_list.append(search_dict)
+            return render(request, 'dashboard/clustal_hist.html', { 'search_list': search_list})
 
     elif request.method == 'POST':
         return render(request, 'dashboard/index.html', { 'year': datetime.now().year, 'title': 'Dashboard', })
