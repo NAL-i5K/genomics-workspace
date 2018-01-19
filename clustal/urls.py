0685685d7859334c9ae5703009d980ef4a919dc0
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-01-19 01:34:33 -0500

    remove user_tasks in urls.py

--- clustal/urls.py
+++ clustal/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     # ex: /clustal/5/
     url(r'^(?P<task_id>[0-9a-zA-Z]+)$', views.retrieve, name='retrieve'),
     url(r'^(?P<task_id>[0-9a-zA-Z]+)/status$', views.status, name='status'),
-    # url(r'^api/', include(router.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # url(r'^api-docs/', include('rest_framework_swagger.urls')),
-    url('^user-tasks/(?P<user_id>[0-9]+)$', views.user_tasks),
     url(r'^manual/', views.manual, name='manual'),
 ]
