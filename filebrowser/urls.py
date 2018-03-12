3dfa300e98d3257184bdfd42a6e22e540f17054a
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-03-12 16:47:59 -0400

    make views are callable in urls.py

--- filebrowser/urls.py
+++ filebrowser/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
+from filebrowser.views import browse, mkdir, upload, rename, delete, versions, _check_file, _upload_file
 
 urlpatterns = [
     # filebrowser urls
-    url(r'^browse/$', 'filebrowser.views.browse', name="fb_browse"),
-    url(r'^mkdir/', 'filebrowser.views.mkdir', name="fb_mkdir"),
-    url(r'^upload/', 'filebrowser.views.upload', name="fb_upload"),
-    url(r'^rename/$', 'filebrowser.views.rename', name="fb_rename"),
-    url(r'^delete/$', 'filebrowser.views.delete', name="fb_delete"),
-    url(r'^versions/$', 'filebrowser.views.versions', name="fb_versions"),
-    url(r'^check_file/$', 'filebrowser.views._check_file', name="fb_check"),
-    url(r'^upload_file/$', 'filebrowser.views._upload_file', name="fb_do_upload"),
+    url(r'^browse/$', browse, name="fb_browse"),
+    url(r'^mkdir/', mkdir, name="fb_mkdir"),
+    url(r'^upload/', upload, name="fb_upload"),
+    url(r'^rename/$', rename, name="fb_rename"),
+    url(r'^delete/$', delete, name="fb_delete"),
+    url(r'^versions/$', versions, name="fb_versions"),
+    url(r'^check_file/$', _check_file, name="fb_check"),
+    url(r'^upload_file/$', _upload_file, name="fb_do_upload"),
 ]
