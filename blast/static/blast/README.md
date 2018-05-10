386944d2144cad4a6375c4df5699fc7858348fa2
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-05-10 09:50:50 -0400

    handle blast static files

--- blast/static/blast/README.md
+++ blast/static/blast/README.md
@@ -2,8 +2,4 @@
 
 * `code-mirror` is for the functionality of bottom left panel of blast result page.
   * current version: [4.5.0](https://github.com/codemirror/CodeMirror/tree/43e88d47524fc1941fe3b74c13987be23e0feb02)
-  * `scripts/codemirror-compressed.js` is compressed version of `codemirror.js` `active-line.js`, and `searchcursor.js` in `scripts/codemirror` through [UglifyJS](https://github.com/mishoo/UglifyJS2).
-    * `active-line.js` and `searchcursor.js` are a little modified (make relative paths correct).
-    * To generate the `scripts/codemirror-compressed.js`, at this folder
-      * `npm install uglify-js@3.3.11 -g`
-      * `uglifyjs ./scripts/codemirror/codemirror.js ./scripts/codemirror/active-line.js ./scripts/codemirror/searchcursor.js -o ./scripts/codemirror-compressed.js --compress` (the order of the input file is important)
+  * include three files in **scripts/**: **codemirror.js**, **active-line.js**, and **searchcursor.js**
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,31 @@ const appScriptConfig = {
   ]
 };
 
+const blastScripts = path.resolve(__dirname, 'blast/static/blast/scripts');
+const blastStyles = path.resolve(__dirname, 'blast/static/blast/css');
+const blastScriptConfig = {
+  entry: path.join(nodeModules, '/jquery-validation/dist/jquery.validate.js'),  // Just a fake entry, we only copy files here
+  plugins: [
+    new CopyWebpackPlugin([
+        { from: path.join(nodeModules, '/jquery-validation/dist/jquery.validate.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/jquery-hoverintent/jquery.hoverIntent.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/jquery-ui-dist/jquery-ui.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/datatables-tabletools/js/dataTables.tableTools.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/datatables.net-colreorder/js/dataTables.colReorder.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/datatables.net-bs/js/dataTables.bootstrap.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/chroma-js/chroma.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/jquery.dragscrollable/dragscrollable.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/backbone/backbone.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/bootstrap-switch/dist/js/bootstrap-switch.js'), to: blastScripts},
+        { from: path.join(nodeModules, '/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css'), to: blastStyles},
+        { from: path.join(nodeModules, '/bootstrap-select/dist/css/bootstrap-select.css'), to: blastStyles},
+        { from: path.join(nodeModules, '/datatables.net-colreorder-dt/css/colReorder.dataTables.css'), to: blastStyles},
+        { from: path.join(nodeModules, '/datatables.net-bs/css/dataTables.bootstrap.css'), to: blastStyles},
+        { from: path.join(nodeModules, '/datatables.net-dt/css/jquery.dataTables.css'), to: blastStyles},
+    ])
+  ]
+};
+
 module.exports = [
-  appScriptConfig,
+  appScriptConfig, blastScriptConfig
 ];
