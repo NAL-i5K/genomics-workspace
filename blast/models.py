358dcbf50cd8982c7e183d92ec93a7dbb30f0392
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-06-16 19:40:46 -0400

    remove blast.organism model

--- blast/models.py
+++ blast/models.py
@@ -28,25 +28,6 @@ class BlastQueryRecord(models.Model):
         verbose_name = 'BLAST result'
 
 
-class OrganismManager(models.Manager):
-    def get_by_natural_key(self, short_name):
-        return self.get(short_name=short_name)
-
-
-class Organism(models.Model):
-    objects = OrganismManager()
-    display_name = models.CharField(max_length=200, unique=True, help_text='Scientific or common name') # shown to user
-    short_name = models.CharField(max_length=20, unique=True, help_text='This is used for file names and variable names in code') # used in code or filenames
-    description = models.TextField(blank=True) # optional
-    tax_id = models.PositiveIntegerField('NCBI Taxonomy ID', null=True, blank=True, help_text='This is passed into makeblast') # ncbi tax id
-
-    def natural_key(self):
-        return (self.short_name,)
-
-    def __unicode__(self):
-        return self.display_name
-
-
 class SequenceTypeManager(models.Manager):
     def get_by_natural_key(self, dataset_type):
         return self.get(dataset_type=dataset_type)
