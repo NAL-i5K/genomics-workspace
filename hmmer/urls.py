5340b871567341fc03aab37b3b714120cf7d2668
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-07-16 17:16:07 -0400

    make manual urls can with slash or without slash

--- hmmer/urls.py
+++ hmmer/urls.py
@@ -3,16 +3,14 @@ from hmmer import views
 # from .api import *
 
 urlpatterns = [
-    # ex: /blast/
+    # ex: /hmmer/
     url(r'^$', views.create, name='create'),
-    # url(r'^iframe$', views.create, {'iframe': True}, name='iframe'),
-    # ex: /blast/5/
-    url(r'^(?P<task_id>[0-9a-zA-Z]+)$', views.retrieve, name='retrieve'),
-    url(r'^(?P<task_id>[0-9a-zA-Z]+)/status$', views.status, name='status'),
-    # url(r'^read_gff3/(?P<task_id>[0-9a-fA-F]*)/*(?P<dbname>[\w\-\|.]*)/*$', views.read_gff3, name='read_gff3'),
+    # ex: /hmmer/task/5/
+    url(r'^task/(?P<task_id>[0-9a-zA-Z]+)$', views.retrieve, name='retrieve'),
+    url(r'^task/(?P<task_id>[0-9a-zA-Z]+)/status$', views.status, name='status'),
     # url(r'^api/', include(router.urls)),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # url(r'^api-docs/', include('rest_framework_swagger.urls')),
     url('^user-tasks/(?P<user_id>[0-9]+)$', views.user_tasks),
-    url('manual/', views.manual, name='manual'),
+    url(r'^manual/$', views.manual, name='manual'),
 ]
