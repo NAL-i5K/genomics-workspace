7040b13ce41246e43b57e210929902a43e157442
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-07-18 14:48:16 -0400

    remove contact and home page of app

--- app/views.py
+++ app/views.py
@@ -1,24 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpRequest
-
-
-def home(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html', {
-            'title': 'Home Page',
-        })
-
-
-def contact(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html', {
-            'title': 'Contact',
-            'message': 'National Agricultural Library',
-        })
 
 
 def handle_404(request):
