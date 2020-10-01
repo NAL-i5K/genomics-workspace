c1c5dde0920f1ad57b1c7deaec57b35d2ae1261f
Vernon Chapman <vernon.chapman@usda.gov>
2020-10-01 10:25:25 -0400

    added send_email function to resultscleanup to test direct cron job with out extrax installed packages

--- app/management/commands/resultscleanup.py
+++ app/management/commands/resultscleanup.py
@@ -9,19 +9,44 @@ from django.core.management.base import BaseCommand
 import django
 import os
 from shutil import rmtree
+from django.core import mail
+
+
 class Command(BaseCommand):
+
+    def send_email(self, body=[]):
+        connection = mail.get_connection()
+        try:
+            
+            
+            subject = f"Test Results Cleanup Management Command - {timezone.now()}"
+            body = "\n".join(body)
+            sender = os.environ.get("HOSTNAME").replace(".","@",1)
+            email_to = [
+            'vernon.chapman@usda.gov',
+            'monica.poelchau@usda.gov',
+            'chris.childers@usda.gov' 
+            ]   
+            mail.EmailMessage(subject, body, sender, email).send()
+        except Eception as e:
+            pass
+        finally:
+            connection.close()
 
     def handle(self,*args,**options):
 
         time_threshold = timezone.now() - timedelta(days=7)
         qr_count = 0
         qr_dirs = 0
-        totoal_qr_count = 0
-        totoal_qr_dirs = 0
+        total_qr_count = 0
+        total_qr_dirs = 0
+        body = []
+
+
 
         for QC in [BlastQueryRecord,ClustalQueryRecord, HmmerQueryRecord]:
             try:
-                records = QC.objects.all().filter(enqueue_date__gte=time_threshold)
+                records = QC.objects.all().filter(enqueue_date__lte=time_threshold)
                 app = QC.__name__.replace("QueryRecord","").lower()
                 task_path = os.path.join(settings.MEDIA_ROOT,f"{app}/task")
 
@@ -39,15 +64,20 @@ class Command(BaseCommand):
                 raise e
 
             if qr_count > 0:
-                totoal_qr_count += qr_count
-                print(f"Located {qr_count} {QC.__name__} records ")
+                total_qr_count += qr_count
+                msg = f"Located {qr_count} {QC.__name__} records "
+                body.append(msg)
+                print(msg)
 
             if qr_dirs > 0: 
-                totoal_qrqr_dirs += qrqr_dirs
-                print(f"Located {qr_dirs} {QC.__name__} task directories ")
+                total_qr_dirs += qr_dirs
+                msg = f"Located {qr_dirs} {QC.__name__} task directories "
+                body.append(msg)
+                print(msg)
 
-        print(f"Total Records:  Located: {totoal_qr_count} {QC.__name__}")
-        print(f"Total Directories Located: {totoal_qr_dirs} {QC.__name__} ")
+        if total_qr_count > 0 or total_qr_dirs > 0) and len(body) > 0:
+            body.append("\n\n")
+            body.append(f"Total Records:  Located: {total_qr_count} {QC.__name__}")
+            body.append(f"Total Directories Located: {total_qr_dirs} {QC.__name__} ")
+            self.send_email(body)
 
-
-            
